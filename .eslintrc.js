0a8af3637c33f0d069c6c4ee3ff7f0dbb94cb4e0
Chris Williams <business@chriswilliams.ca>
2025-04-19 13:48:41 -0400

    ok

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'react-app',
+    'react-app/jest',
+    'prettier'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    'prettier'
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'no-unused-vars': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-undef': 'error',
+    'no-unused-expressions': 'off',
+    'no-sequences': 'off',
+    'no-restricted-globals': 'off',
+    'no-empty': 'off',
+    'no-useless-escape': 'off',
+    'no-case-declarations': 'off',
+    'no-async-promise-executor': 'off',
+    'no-use-before-define': 'off'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  ignorePatterns: ['**/*.min.js', '**/dist/*', '**/build/*', 'node_modules']
+}; 
